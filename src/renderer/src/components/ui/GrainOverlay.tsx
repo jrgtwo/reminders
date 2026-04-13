22e00f5c7635e35f612f7474ceab4194723d8a02
jrgtwo <jonathanraygarcia@gmail.com>
2026-04-13 13:14:35 -0700

    feat: more grain

--- src/renderer/src/components/ui/GrainOverlay.tsx
+++ src/renderer/src/components/ui/GrainOverlay.tsx
@@ -1,31 +1,25 @@
 export default function GrainFilter() {
   return (
-    <>
-      {/* Hidden SVG filter definitions */}
-      <svg
-        aria-hidden="true"
-        style={{ position: 'absolute', width: 0, height: 0, overflow: 'hidden' }}
-      >
-        <filter id="grain-fine">
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency="0.70"
-            numOctaves="3"
-            stitchTiles="stitch"
-          />
-        </filter>
-        <filter id="grain-coarse">
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency="0.25"
-            numOctaves="2"
-            stitchTiles="stitch"
-          />
-        </filter>
-      </svg>
-
-      {/* Layer 3: film grain on top of everything */}
-      <div className="grain-film" aria-hidden="true" />
-    </>
+    <svg
+      aria-hidden="true"
+      style={{ position: 'absolute', width: 0, height: 0, overflow: 'hidden' }}
+    >
+      <filter id="grain-fine">
+        <feTurbulence
+          type="fractalNoise"
+          baseFrequency="0.70"
+          numOctaves="3"
+          stitchTiles="stitch"
+        />
+      </filter>
+      <filter id="grain-coarse">
+        <feTurbulence
+          type="fractalNoise"
+          baseFrequency="0.25"
+          numOctaves="2"
+          stitchTiles="stitch"
+        />
+      </filter>
+    </svg>
   )
 }
